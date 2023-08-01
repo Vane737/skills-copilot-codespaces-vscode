e8a22062a1bbf164cec50cb792ed5802d8e5f393
Vivian Flores <77525733+Vane737@users.noreply.github.com>
2023-08-01 15:50:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,51 +1,31 @@
 // create web server
-// node comments.js
-// http://localhost:8080
-// http://localhost:8080/comments
-// http://localhost:8080/comments/1
 
-// create web server
-const http = require('http');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
+// import module
+const express = require('express');
+const router = express.Router();
+const commentsController = require('../controllers/commentsController');
+const { body, validationResult } = require('express-validator');
 
-const server = http.createServer(function (req, res) {
-  // get the path name
-  const pathname = url.parse(req.url).pathname;
-  // get the file extension
-  const extname = path.extname(pathname);
-  // get the file type
-  const type = getContentType(extname);
-  // get the file path
-  const filepath = path.join(__dirname, pathname);
-  // get the file content
-  fs.readFile(filepath, function (err, data) {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('File not found');
-    } else {
-      res.writeHead(200, { 'Content-Type': type });
-      res.end(data);
-    }
-  });
-});
+// create route
+router.get('/', commentsController.index);
 
-// start web server
-server.listen(8080, function () {
-  console.log('Server is running on port 8080');
-});
+router.get('/create', commentsController.create);
 
-// get the file type
-function getContentType(extname) {
-  switch (extname) {
-    case '.html':
-      return 'text/html';
-    case '.css':
-      return 'text/css';
-    case '.js':
-      return 'text/javascript';
-    default:
-      return 'text/plain';
-  }
-} 
+router.post('/store', [
+    body('name').not().isEmpty().withMessage('Name is required'),
+    body('email').not().isEmpty().withMessage('Email is required'),
+    body('content').not().isEmpty().withMessage('Content is required'),
+], commentsController.store);
+
+router.get('/:id/edit', commentsController.edit);
+
+router.post('/update', [
+    body('name').not().isEmpty().withMessage('Name is required'),
+    body('email').not().isEmpty().withMessage('Email is required'),
+    body('content').not().isEmpty().withMessage('Content is required'),
+], commentsController.update);
+
+router.get('/:id/delete', commentsController.delete);
+
+// export router
+module.exports = router;
